5160fea6f948d2796e99cc58f29af3d048693b90
Mike Santora <santoram@oregonstate.edu>
2026-04-27 20:42:57 -0400

    4.27 - Added more to table query

--- cs340_ddl.sql
+++ cs340_ddl.sql
@@ -14,54 +14,60 @@ Assigment Part 1: Create a DDL file that imports on the database server used to 
 SET FOREIGN_KEY_CHECKS=0;
 SET AUTOCOMMIT = 0;
 
+
 -- Create Customers table
 CREATE OR REPLACE TABLE Customers (
-    customer_id int AUTO_INCREMENT,
-    name varchar(145),
-    email varchar(255),
-    created_date datetime,
-    PRIMARY KEY (customer_id)
+    customer_id INT NOT NULL AUTO_INCREMENT,
+    name varchar(145) NOT NULL,
+    email varchar(255) NOT NULL,
+    created_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (customer_id),
+    ON DELETE RESTRICT
 );
 
 -- Create Orders table
 CREATE OR REPLACE TABLE Orders (
-    order_id int AUTO_INCREMENT,
-    created_date datetime,
-    customer_id int,
-    employee_id int,
+    order_id INT NOT NULL AUTO_INCREMENT,
+    created_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    customer_id INT NOT NULL,
+    employee_id INT NULL,
     PRIMARY KEY (order_id),
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
-    FOREIGN KEY (employee_id) REFERENCES Employees (employee_id)
+    FOREIGN KEY (employee_id) REFERENCES Employees (employee_id),
+    ON DELETE RESTRICT
 );
 
 -- Create Employees table
 CREATE OR REPLACE TABLE Employees (
-    employee_id int AUTO_INCREMENT,
-    code smallint,
-    name varchar(145),
-    email varchar(255),
-    PRIMARY KEY (employee_id)
+    employee_id INT NOT NULL AUTO_INCREMENT,
+    code SMALLINT UNSIGNED NOT NULL UNIQUE,
+    name varchar(145) NOT NULL,
+    email varchar(255) NOT NULL UNIQUE,
+    PRIMARY KEY (employee_id),
+    ON DELETE RESTRICT
 );
 
 -- Create Products table
 CREATE OR REPLACE TABLE Products (
-    product_id int AUTO_INCREMENT,
-    name varchar(145),
-    price decimal(6,2),
-    quantity_on_hand smallint,
-    PRIMARY KEY (product_id)
+    product_id INT NOT NULL AUTO_INCREMENT,
+    name varchar(145) NOT NULL,
+    price decimal(6,2) NOT NULL,
+    quantity_on_hand SMALLINT UNSIGNED NOT NULL,
+    PRIMARY KEY (product_id),
+    ON DELETE RESTRICT
 );
 
 -- Create Order_Items table
 CREATE OR REPLACE TABLE Order_Items (
-    order_items_id int AUTO_INCREMENT,
-    order_id int,
-    product_id int,
-    quantity tinyint,
-    sale_price decimal(6,2),
+    order_items_id INT NOT NULL AUTO_INCREMENT,
+    order_id INT NOT NULL,
+    product_id INT NOT NULL,
+    quantity TINYINT NOT NULL,
+    sale_price decimal(6,2) NOT NULL,
     PRIMARY KEY (order_items_id),
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
-    FOREIGN KEY (product_id) REFERENCES Products(product_id)
+    FOREIGN KEY (product_id) REFERENCES Products(product_id),
+    ON DELETE RESTRICT
 );
 
 -- turn on commits and foreign key checks
